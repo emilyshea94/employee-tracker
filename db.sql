31baf6dace11b8787c50df6b68bee264e0cf0697
emily shea <emilyshea947@gmail.com>
2024-02-01 20:06:53 -0500

    added schema code

--- db.sql
+++ db.sql
@@ -13,3 +13,12 @@ title VARCHAR(30)
 salary DECIMAL
 department_id INT
 );
+
+CREATE TABLE employee(
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30)
+last_name VARCHAR(30)
+role_id INT
+manager_id INT
+);
+
