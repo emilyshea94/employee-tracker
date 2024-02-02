31635b67bdbefe978dfd028d7ff16da6dbe8eac7
emily shea <emilyshea947@gmail.com>
2024-02-01 20:04:07 -0500

    added db.sql

---
+++ db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+name VARCHAR(30)
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(30)
+salary DECIMAL
+department_id INT
+);
---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer= require("inquirer")
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'sage123',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee.db database.`)
+  );
+  
+  const sql = ` INTO movies (movie_name)
+    VALUES (?)`;
+  const params = [body.movie_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
